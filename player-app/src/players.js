fff5f35593f5e15c16ed998db0cc3ea22a2a194c
[ahmed ben said ] <ahmedbensaidbensaid@gmail.com>
2024-06-03 15:39:23 +0100

    addingfiles

---
+++ player-app/src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentine",
+      jerseyNumber: 30,
+      age: 35,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al Nassr",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Neymar Jr",
+      team: "Paris Saint-Germain",
+      nationality: "Brazilian",
+      jerseyNumber: 10,
+      age: 30,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "French",
+      jerseyNumber: 7,
+      age: 23,
+      imageUrl: "https://via.placeholder.com/150"
+    }
+  ];
+  
+  export default players;
+  
